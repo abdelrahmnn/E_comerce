7dd1cae91eb1e01bcda24fc4eb7617a245595762
Your Name <you@example.com>
2023-04-26 08:13:57 -0700

    call const color

--- lib/shared/colors.dart
+++ lib/shared/colors.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
 const BTNpink = Color.fromARGB(255, 241, 39, 100);
+const BTNgreen = Color.fromARGB(255, 73, 39, 105);
+const appbargreen = Color.fromARGB(255, 241, 141, 95);

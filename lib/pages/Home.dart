cd688299d7657b930b9efa43e5c704d33376c5e7
Your Name <you@example.com>
2023-04-26 08:43:47 -0700

    the padding

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -16,6 +16,10 @@ class Home extends StatelessWidget {
               Stack(
                 children: [
                   Container(child: Text("8")),
+                  padding: EdgeInsets.all(5),
+    decoration: BoxDecoration(
+    color: Color.fromARGB(211, 164, 255, 193),
+    shape: BoxShape.circle)
                   IconButton(
                     onPressed: () {},
                     icon: Icon(Icons.add_shopping_cart),

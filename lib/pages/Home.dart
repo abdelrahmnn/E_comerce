b817b4d50a3b5906815a0c585a762e3db9cfd520
Your Name <you@example.com>
2023-04-26 09:16:00 -0700

    for image

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -14,6 +14,7 @@ class Home extends StatelessWidget {
           child:Column(
             children: [
               UserAccountsDrawerHeader(
+                backgroundImage: AssetImage("assets/img/ali.jpg")),
                 accountEmail: Text("ali@yahoo.com"),
                 accountName: Text("ali Hassan",
  style: TextStyle(

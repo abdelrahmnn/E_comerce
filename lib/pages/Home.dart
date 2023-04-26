6c9b6138cbcdf0d6884465435f0e85637606f4f3
Your Name <you@example.com>
2023-04-26 08:04:13 -0700

    backgroundColor: to control appbarcolor

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -7,7 +7,36 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("data"),
+ backgroundColor: Color.fromARGB(255, 90, 76, 141),
+ title: Text("Home"),
+ actions: [
+  Row(
+    children: [
+    Stack(
+      children: [
+      Container(
+    child: Text(
+    "8",
+    style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+    ),
+    padding: EdgeInsets.all(5),
+    decoration: BoxDecoration(
+    color: Color.fromARGB(211, 164, 255, 193),
+    shape: BoxShape.circle)),
+  
+ IconButton(
+ onPressed: () { },
+ icon: Icon(Icons.add_shopping_cart)),
+ ],
+ ),
+ Padding(
+ padding: const EdgeInsets.only(right: 12),
+ child: Text("\$ 128"),
+ )
+ ],
+ )
+ ],
+ ),
       ),
     );
   }

f9dfefb71913e63a7a023778f4225e5b8d78badd
Your Name <you@example.com>
2023-04-26 05:16:09 -0700

    filled color  to shade text filed

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -22,7 +22,7 @@ class Login extends StatelessWidget {
               // To delete borders
               enabledBorder: OutlineInputBorder(borderSide: Divider.createBorderSide(context),),
               focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 243, 191, 33),),),
-              // fillColor: Colors.red,
+              fillColor: Colors.red,
               filled: true,
               contentPadding: const EdgeInsets.all(8),
             ))],

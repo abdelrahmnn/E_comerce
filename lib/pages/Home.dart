b86813574b43e3fb9f2b409591ff011d6d5c69e8
Your Name <you@example.com>
2023-04-26 08:18:03 -0700

    text field

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:e_comerce_app/shared/colors.dart';
 import 'package:flutter/material.dart';
@@ -10,7 +10,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [],
+        actions: [Text("\$ 13")],
         backgroundColor: appbargreen,
         title: Text("home"),
       ),

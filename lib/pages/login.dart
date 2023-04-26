9c9e2a31cc04f89a0ca35a9aea091cf669a357c6
Your Name <you@example.com>
2023-04-26 04:25:45 -0700

    the column take the propertiy that named children

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -6,7 +6,9 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      body: Column(
+        children: [],
+      ),
     );
   }
 }

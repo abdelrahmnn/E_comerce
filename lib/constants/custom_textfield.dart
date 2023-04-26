b308dd3d904cbaf2aafbbe97977a9b85e1d9a77b
Your Name <you@example.com>
2023-04-26 05:48:17 -0700

    call the libarary

---
+++ lib/constants/custom_textfield.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget {
+  const MyTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+        keyboardType: TextInputType.emailAddress,
+        obscureText: false,
+        decoration: InputDecoration(
+          hintText: "Enter Your email : ",
+          // To delete borders
+          enabledBorder: OutlineInputBorder(
+            borderSide: Divider.createBorderSide(context),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Color.fromARGB(255, 243, 191, 33),
+            ),
+          ),
+          fillColor: Colors.red,
+          filled: true,
+          contentPadding: const EdgeInsets.all(10),
+        ));
+  }
+}
